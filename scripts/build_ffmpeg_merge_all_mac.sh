27fd12dcf8a947d31e340dd56cbab26101961e54
victor <zhanghaifeng903@sina.com>
2019-07-27 12:08:54 +0800

    git add mac build scrip

---
+++ scripts/build_ffmpeg_merge_all_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for arch in armeabi armeabi-v7a arm64-v8a x86 x86_64
+do
+    bash build_ffmpeg_merge_mac.sh $arch
+done
+
+echo "armeabi armeabi-v7a arm64-v8a x86 x86_64 build all success"
